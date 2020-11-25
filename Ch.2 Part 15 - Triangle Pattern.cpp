2aa4e813fb1cd1b92dbf9cf0920a763e3144a118
Rachel <72714577+residentevil35@users.noreply.github.com>
2020-11-24 19:13:33 -0600

    Add files via upload

---
+++ Ch.2 Part 15 - Triangle Pattern.cpp
@@ -0,0 +1,26 @@
+ /********************************************************************************
+  * Rachel Coughanour                                                            *
+  * Starting Out with C++ From Control Structures Through Objects 8th Edition    *              
+  * Programming Challenges - Chapter 2                                           *
+  * 15) Triangle Pattern - Page 81                                               *
+  *                                                                              *
+  * Description: Write a program that displays the following pattern on the      *
+  * screen:                                                                      *
+  *           *                                                                  *
+  *          ***                                                                 *
+  *	        *****                                                                *
+  ********************************************************************************/
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+   cout<<endl<<endl;
+   cout<<"   *   "<<endl;
+   cout<<"  ***   "<<endl;
+   cout<<" *****   "<<endl;
+    return 0;
+}
+  
